be8970789aa3403e9ce16568b7ea3ea68ab7cd56
Natalia C <natalia.calt@gmail.com>
2018-10-24 13:19:56 -0600

    Routing, Components are set

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class AboutPage extends React.Component{
+  render(){
+    return (
+      <div className="jumbotron">
+        <h1>About</h1>
+        <p>This application uses React, Redux, and React Router with ES6</p>
+      </div>
+    );
+  }
+}
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 //this file is - Entry Point!
-console.log("hi");
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes.js';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+  <Router history={ browserHistory } routes={ routes } />,
+  document.getElementById('app')
+);
