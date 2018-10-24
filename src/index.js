787a64515b83c4a11253527feb556548b22a684c
Natalia C <natalia.calt@gmail.com>
2018-10-24 16:37:20 -0600

    corrections

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 //this file is - Entry Point!
 import 'babel-polyfill';
 import React from 'react';
-import { render } from 'react-dom';
-import configureStore from '.store/configureStore';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
 import routes from './routes.js';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -13,7 +13,7 @@ const store = configureStore();
 
 render(
   <Provider store={store}>
-    <Router history={ browserHistory } routes={ routes } />
+    <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app')
 );
